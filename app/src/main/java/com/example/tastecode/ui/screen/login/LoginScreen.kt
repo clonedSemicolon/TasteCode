55b754f7b108362f90d190be58e3033fbf684fe2
Avishak-cb <g023njset1ilhjhfsahgrc0e0e16fp@bots.bitbucket.org>
2024-11-04 21:55:48 -0500

    folder structure updated

---
+++ app/src/main/java/com/example/tastecode/ui/screen/login/LoginScreen.kt
@@ -0,0 +1,9 @@
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.example.tastecode.R
+
+@Composable
+fun LoginScreen(){
+    Image(painterResource(R.drawable.rectangle_38),"content description")
+}
