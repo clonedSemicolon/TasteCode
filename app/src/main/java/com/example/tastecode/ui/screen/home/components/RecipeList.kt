a4c29920e757e27e0b14b1d104db870cac128bc8
Avishak-cb <g023njset1ilhjhfsahgrc0e0e16fp@bots.bitbucket.org>
2024-11-24 22:12:01 -0500

    Recipe Card Design and HomeScreen design updated.

--- app/src/main/java/com/example/tastecode/ui/screen/home/components/RecipeList.kt
+++ app/src/main/java/com/example/tastecode/ui/screen/home/components/RecipeList.kt
@@ -1,8 +1,14 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.tastecode.business.route.Screen
@@ -14,10 +20,15 @@ fun RecipeList(
 ) {
     val recipes by viewModel.recipes.observeAsState(emptyList())
 
-    LazyColumn {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
         items(recipes) { recipe ->
             RecipeItem(
-                recipe,
+                recipe = recipe,
                 onClick = {
                     SingleRecipeData.recipeData = RecipeData(
                         title = recipe.name,
